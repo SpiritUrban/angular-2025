0fa7d90e19cfa8f0e04acd82fcdb37e7f6befe6e
Vitaliy Dyachuk <shadespiritenator@gmail.com>
2025-02-13 02:21:15 +0200

    add docs

--- first-2025/src/app/app.component.ts
+++ first-2025/src/app/app.component.ts
@@ -1,15 +1,17 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { FormsModule } from '@angular/forms';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, FormsModule, CommonModule],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
 export class AppComponent {
   title = 'first-2025';
-  aditionalInfo = 'Super Mega Power';
+  isShowInput = true;
 
   changeTitle(){
     this.title = 'Super Mega Power';
