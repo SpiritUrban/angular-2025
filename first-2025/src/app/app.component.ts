c6723d30ce9c9df423c72d4580ee78cfa7a4630f
Vitaliy Dyachuk <shadespiritenator@gmail.com>
2025-02-19 16:06:34 +0200

    perfomance optimization test

--- first-2025/src/app/app.component.ts
+++ first-2025/src/app/app.component.ts
@@ -1,20 +1,43 @@
-import { Component } from '@angular/core';
+import { Component, DoCheck } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { FormsModule } from '@angular/forms';
 import { CommonModule } from '@angular/common';
+import { TestComponent } from './components/test/test.component';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, FormsModule, CommonModule],
+  imports: [RouterOutlet, FormsModule, CommonModule, TestComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
-export class AppComponent {
+export class AppComponent implements DoCheck {
   title = 'first-2025';
   isShowInput = true;
+  itemsBufer = ['item1', 'item2', 'item3'];
+  items = ['item1', 'item2', 'item3'];
+
+  ngOnInit() {
+    console.log('TestComponent initialized from AppComponent');
+  }
 
   changeTitle(){
     this.title = 'Super Mega Power';
   }
+
+  ngDoCheck(): void {
+    console.log('---ngDoCheck');
+     if (this.itemsBufer.length > 10000) {
+      console.log('CLEARING ITEMS');
+      this.itemsBufer = [];
+    }
+    for (let i = 0; i < 100; i++) {
+      this.itemsBufer.push('' + i);
+      console.log('FOR', this.itemsBufer.length);
+
+    }
+    this.items = [...this.itemsBufer];
+  }
+
+
 
 }
---
+++ first-2025/src/app/components/test/test.component.ts
@@ -0,0 +1,42 @@
+import {
+  Component, OnInit, AfterContentInit, AfterContentChecked,
+  ChangeDetectionStrategy, Input, DoCheck
+} from '@angular/core';
+import { CommonModule } from '@angular/common';
+@Component({
+  selector: 'app-test',
+  imports: [CommonModule],
+  templateUrl: './test.component.html',
+  styleUrl: './test.component.scss',
+  changeDetection: ChangeDetectionStrategy.OnPush
+})
+export class TestComponent implements OnInit, AfterContentInit, AfterContentChecked {
+
+  @Input() items: any = ['item1', 'item2', 'item3'];
+
+  constructor() { }
+
+  ngOnInit() {
+    console.log('TestComponent initialized');
+    console.log('---ngOnInit');
+    for (let i = 0; i < 10000; i++) {
+      const x = i;
+      console.log('FOR', x);
+    }
+  }
+
+  ngAfterContentInit() {
+    console.log('---ngAfterContentInit');
+  }
+
+  ngAfterContentChecked() {
+    console.log('---ngAfterContentChecked !!!!');
+  }
+
+  trackByIndex(index: number, item: string): number {
+    return index;  // Track items by their index
+  }
+
+
+
+}
